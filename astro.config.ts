e4deee5d11d8756874954e176011b86f0848042c
ZAREZOV2 <tomonsteru@gmail.com>
2024-03-09 16:32:58 +0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,6 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ZAREZOV2.github.io',
-  base: '/bmnewsrtanks',
   integrations: [
     tailwind({
       applyBaseStyles: false,
